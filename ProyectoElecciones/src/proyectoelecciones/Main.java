15178b16def9a66b72686f47194d74c71704252d
Joelyx <joelyx96@gmail.com>
2023-02-09 20:09:28 +0000

    Init main

---
+++ ProyectoElecciones/src/proyectoelecciones/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoelecciones;
+
+/**
+ *
+ * @author Joel Hernández Martín joelyx96@gmail.com
+ */
+public class Main {
+    public static void main(String[] args) {
+        // aqui va todo el main
+    }
+}
