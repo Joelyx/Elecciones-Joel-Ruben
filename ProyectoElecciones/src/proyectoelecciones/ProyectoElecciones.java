f5f8512e600d5b092e8936bc3837416ab264dd8b
Joelyx <joelyx96@gmail.com>
2023-02-09 16:18:51 +0000

    Inicializacion del proyecto

---
+++ ProyectoElecciones/src/proyectoelecciones/ProyectoElecciones.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoelecciones;
+
+/**
+ *
+ * @author Joel Hernández Martín joelyx96@gmail.com
+ */
+public class ProyectoElecciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
