5d1485f0d5d237a4ae2101b78dbff0d1aa7d7daa
poyben <poybenglez@gmail.com>
2023-02-10 11:44:53 +0000

    Feat: added basic fuctionality to Eleeciones and Partido

--- ProyectoElecciones/src/proyectoelecciones/Main.java
+++ ProyectoElecciones/src/proyectoelecciones/Main.java
@@ -4,12 +4,75 @@
  */
 package proyectoelecciones;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
 /**
  *
  * @author Joel Hernández Martín joelyx96@gmail.com
  */
 public class Main {
+
+    public void addPartido(String nombre, int votos, String presidente){
+        Partido partido = new Partido(nombre, votos, presidente);
+        partidos.add(partido);
+    }
+    
+    public static void crearElecciones(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce cuantos diputados va a tener:");
+                    int diputados = sc.nextInt();sc.nextLine();
+                    Date fecha = new Date();
+                    pulir fecha
+                    
+                    Elecciones nuevasElecciones = new Elecciones(diputados,fecha);
+                    listaElecciones.add(nuevasElecciones);
+    }
+    
+    private static ArrayList<Elecciones> listaElecciones = new ArrayList<Elecciones>();
+
+    public static void listarElecciones() {
+        for (int i = 0; i < listaElecciones.size(); i++) {
+            System.out.println(i + 1 + ".- " + listaElecciones.get(i));
+        }
+    }
+
     public static void main(String[] args) {
-        // aqui va todo el main
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Menu elecciones  \n"
+                + "¿Que quieres hacer?  \n"
+                + "1. Ver elecciones. \n"
+                + "2. Crear elecciones. \n"
+                + "3. Ver estadisticas \n"
+                + "4. Añadir partido "
+                + "5. Salir");
+        int opcion = sc.nextInt();
+        boolean salir = false;
+
+        do {
+            switch (opcion) {
+                case 1:
+                    if(listaElecciones.isEmpty()){
+                        System.out.println("Aun no hay elecciones creadas.");
+                    }else{
+                        listarElecciones();
+                    }
+                    break;
+                case 2:
+                    crearElecciones();
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    salir = true;
+                    break;
+            }
+        } while (salir == false);
+
     }
 }
