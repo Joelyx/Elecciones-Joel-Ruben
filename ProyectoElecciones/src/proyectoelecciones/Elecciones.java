3ec8c01fe8bb66d7f1a6a4fb5598b67f55c71eff
Joel Hernández <joelyx96@gmail.com>
2023-02-10 11:40:03 +0000

    Feat: added basic fuctionality to Eleeciones and Partido

--- ProyectoElecciones/src/proyectoelecciones/Elecciones.java
+++ ProyectoElecciones/src/proyectoelecciones/Elecciones.java
@@ -4,10 +4,40 @@
  */
 package proyectoelecciones;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author Joel Hernández Martín joelyx96@gmail.com
  */
 public class Elecciones {
+    private ArrayList<Partido> partidos;
+    private int diputadosTotales;
+    private Date fecha;
+    
+    public Elecciones(){}
+    
+    public Elecciones(int diputados, Date fecha){
+        this.diputadosTotales=diputados;
+        this.fecha=fecha;
+    }
+    
+    public void addPartido(String nombre, int votos, String presidente){
+        Partido partido = new Partido(nombre, votos, presidente);
+        partidos.add(partido);
+    }
+
+    @Override
+    public String toString() {
+        String res="Elecciones del " + fecha + "";
+        for (int i = 0; i < partidos.size(); i++) {
+            res+="\n Patido "+i+") "+partidos.get(i);
+        }
+        
+        return res;
+    }
+    
+    
     
 }
