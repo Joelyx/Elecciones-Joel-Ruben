87a505afe50da488f22586b444c06ca4f3a03137
poyben <poybenglez@gmail.com>
2023-02-16 17:15:01 +0000

    Creo que acabado

--- ProyectoElecciones/src/Calculadora/Main.java
+++ ProyectoElecciones/src/Calculadora/Main.java
@@ -4,7 +4,9 @@
  */
 package Calculadora;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  *
@@ -13,6 +15,10 @@ import java.util.Scanner;
  */
 public class Main {
 
+    public static Caculadora calculadora = new Caculadora();
+    
+    
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean salir = false;  
@@ -29,12 +35,24 @@ public class Main {
 
             switch(opcion){
                 case 1:
+                    System.out.println("Escribe la suma que quieras hacer: Ejemplo '2 3 +'");
+                    String suma = sc.nextLine();
+                    calculadora.calcular(suma);
                     break;
                 case 2:
+                    System.out.println("Escribe la resta que quieras hacer: Ejemplo '7 2 -'");
+                    String resta = sc.nextLine();
+                    calculadora.calcular(resta);
                     break;
                 case 3:
+                    System.out.println("Escribe la multiplicacion que quieras hacer: Ejemplo '4 3.2 *'");
+                    String multi = sc.nextLine();
+                    calculadora.calcular(multi);
                     break;
                 case 4:
+                    System.out.println("Escribe la division que quieras hacer: Ejemplo '6 3 /'");
+                    String divi = sc.nextLine();
+                    calculadora.calcular(divi);
                     break;
                 case 0:
                     salir = true;
