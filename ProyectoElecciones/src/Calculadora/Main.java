81be8027005d6c4eecb3cdfddd5c666cf955115c
poyben <poybenglez@gmail.com>
2023-02-16 16:53:33 +0000

    Feature: Esqueleto del menu

--- ProyectoElecciones/src/Calculadora/Main.java
+++ ProyectoElecciones/src/Calculadora/Main.java
@@ -4,10 +4,42 @@
  */
 package Calculadora;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Joel Hernández Martín joelyx96@gmail.com
+ * @author Ruben Gonzalez Sabina poybenglez@gmail.com
  */
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean salir = false;  
+        do {
+            System.out.println("Menu calculadora \n "
+                    + "Que quiere hacer? \n"
+                    + "1.- Sumar \n"
+                    + "2.- Restar \n"
+                    + "3.- Multiplicar \n"
+                    + "0.- Salir \n");
+
+            int opcion = sc.nextInt();
+
+            switch(opcion){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 0:
+                    salir = true;
+                    break;
+            } 
+            }
+            while (salir == false);
+        }
     
-}
+    
+    }
